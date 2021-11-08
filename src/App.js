64cf5d560d53583ade4a319ddecf19e026dd8e6e
P-ertho <ibisi@frro.utn.edu.ar>
2021-11-08 15:34:56 -0300

    rollback

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import { Storage } from 'aws-amplify'
+import { Storage} from 'aws-amplify'
 
-class App extends Component {
-  state = {fileUrl: '', file: '', filename: ''}
-  handleChange = e => {
-    const file = e.target.files[0]
-    this.setState({
-      fileUrl: URL.createObjectURL(file),
-      file,
-      filename: file.name
-    })
-  }
-
-  saveFile = () =>{
-    Storage.put(this.state.filename, this.state.file)
-    .then(()=> {
-      console.log('todo bien cargando imagen')
-      this.setState({fileUrl: '', file: '', filename: ''})
-    })
-    .catch(err => {
-      console.log('error uploading file', err)
-    })
-  }
-  render(){
+function App() {
   return (
     <div className="App">
-      <text> Ingrese CBU </text>
-      <input type='text'/>
-      <form className="form-horizontal">
-        <input type='file' onChange={this.handleChange}/>
-        <img src={this.state.fileUrl}/>
-      </form>
-        <button onClick={this.saveFile}>Confirmar</button>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
       <AmplifySignOut />
     </div>
   );
-}
 }
 
 export default withAuthenticator(App);
